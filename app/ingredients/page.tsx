8adbbf719d060d8281333c9fe6529150e0e62f18
nghillaz <nghillaz@gmail.com>
2024-05-04 16:05:29 -0500

    basic navigation

---
+++ app/ingredients/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+import { Common, NavigationBarSelections } from "../Common";
+
+export default function Ingredients() {
+  return (
+    <>
+      <Common selectedItem={NavigationBarSelections.Ingredients} />
+    </>
+  );
+}
