742a0149703f648d1489b285b02ecaa31ea0e006
Nathan Hill <nhill@epic.com>
2024-05-08 10:19:23 -0500

    add actual favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/icon.ico" sizes="any"/>
+      <link rel="icon" href="/icon.png" sizes="any"/>
       <body className={inter.className}>{children}</body>
     </html>
   );
