de587dc563b31f0d9615be6df63ada1afa144363
Nathan Hill <nhill@epic.com>
2024-05-10 11:48:27 -0500

    better interactivity

--- app/_components/button.tsx
+++ app/_components/button.tsx
@@ -2,6 +2,7 @@ import Image, { StaticImageData } from "next/image";
 import images from "../images/_index";
 import "./button.css";
 import { classList } from "./_common";
+import { KeyboardEventHandler, MouseEventHandler, useCallback, useRef, KeyboardEvent as ReactKeyboardEvent, MouseEvent as ReactMouseEvent } from "react";
 
 export function Button(props: {
     image: StaticImageData;
@@ -12,14 +13,62 @@ export function Button(props: {
     disabled?: boolean
 }
 ) {
+    const buttonEl = useRef<HTMLDivElement>(null);
+
+    const onPress: KeyboardEventHandler & MouseEventHandler = useCallback((event: ReactKeyboardEvent & ReactMouseEvent) => {
+        if (!buttonEl.current) { return; }
+
+        if (event.key !== undefined) {
+            switch (event.key) {
+                case "Enter":
+                case "Spacebar":
+                case " ":
+                    buttonEl.current.classList.add("clicked");
+                    break;
+            }
+        }
+
+        if (event.button !== undefined) {
+            console.log("button: " + event.button);
+            if (event.button === 0) {
+                buttonEl.current.classList.add("clicked");
+            }
+        }
+    }, []);
+
+    const onRelease: KeyboardEventHandler & MouseEventHandler = useCallback((event: ReactKeyboardEvent & ReactMouseEvent) => {
+        if (!buttonEl.current) { return; }
+        if (event.key !== undefined) {
+            switch (event.key) {
+                case "Enter":
+                case "Spacebar":
+                case " ":
+                    buttonEl.current.classList.remove("clicked");
+                    props.onSelect();
+                    break;
+            }
+        }
+
+        if (event.button !== undefined) {
+            if (event.button === 0) {
+                buttonEl.current.classList.remove("clicked");
+            }
+        }
+    }, []);
+
     return (
-        <div
+        <div tabIndex={0} ref={buttonEl}
             className={classList([
                 "button clickable highlightable",
                 props.isSelected ? "selected" : "",
                 props.disabled ? "disabled" : "",
                 props.className ?? "",
             ])}
+            onKeyDown={onPress}
+            onKeyUp={onRelease}
+            onMouseDown={onPress}
+            onMouseUp={onRelease}
+            onMouseLeave={onRelease}
             onClick={props.disabled ? undefined : event => { props.onSelect(); event.stopPropagation(); }}
         >
             <div className="buttonIcon">
--- app/_components/popup.tsx
+++ app/_components/popup.tsx
@@ -8,15 +8,35 @@ export function Popup(props: {
   onClose: Function;
   content: JSX.Element;
 }) {
+  useEffect(() => {
+    const escapeListener: EventListener = (event: Event) => {
+      const keyboardEvent = event as KeyboardEvent;
+      if (keyboardEvent?.key === "Escape") {
+        props.onClose();
+      };
+    };
+
+    window.addEventListener("keydown", escapeListener);
+
+    return () => {
+      window.removeEventListener("keydown", escapeListener)
+    };
+  }, []);
+
   return (
     <div
       className={classList(["popupContainer", props.visible ? "visible" : ""])}
       onClick={(event) => event.stopPropagation()}
+      onKeyDown={event => {
+        if (event.key === "Escape") {
+          props.onClose();
+        }
+      }}
     >
       <div className="popup">
         <div className="popupContent">
-        <CloseButton onClick={props.onClose} />
-        {props.content}
+          <CloseButton onClick={props.onClose} />
+          {props.content}
         </div>
       </div>
     </div>
